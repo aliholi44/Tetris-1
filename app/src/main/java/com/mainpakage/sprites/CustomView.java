c23f5ec4317f2b1884877416509151f3af9e22e2
IhoFenixMFS <iho-ladamadeltiempo@live.com>
2019-10-02 19:31:36 +0200

    Interfaz conectada a canvas y clases rehechas

--- app/src/main/java/com/mainpakage/sprites/CustomView.java
+++ app/src/main/java/com/mainpakage/sprites/CustomView.java
@@ -4,42 +4,80 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.util.AttributeSet;
 import android.view.View;
-import com.mainpakage.sprites.TetrixPieces.*;
+import android.widget.ImageView;
+
+import com.mainpakage.sprites.TetrixPieces.CubePiece;
+import com.mainpakage.sprites.TetrixPieces.JPiece;
+import com.mainpakage.sprites.TetrixPieces.LPiece;
+import com.mainpakage.sprites.TetrixPieces.SPiece;
+import com.mainpakage.sprites.TetrixPieces.TPiece;
+import com.mainpakage.sprites.TetrixPieces.TetrixPiece;
+import com.mainpakage.sprites.TetrixPieces.ZPiece;
+
+import java.util.List;
 
 public class CustomView extends View {
 
     Bitmap bmp;
-    CubePiece cp;
-    LinePiece lp;
-    SPiece sp;
-    TPiece tp;
-    ZPiece zp;
+    TetrixPiece tp;
+    int score;
+    private final int lineScore=30;
+    List<TetrixPiece> piezas;
+    private TetrixPiece nextPiece;
+    private TetrixPiece activePiece;
 
 
-    public CustomView(Context context){
-        super(context);
-
+    public CustomView(Context context, AttributeSet attrs){
+        super(context,attrs);
         bmp = BitmapFactory.decodeResource(getResources(), R.drawable.spritetest2);
-        cp = new CubePiece(bmp,this);
-        lp = new LinePiece(bmp, this);
-        sp = new SPiece(bmp,this);
         tp = new TPiece(bmp,this);
-        zp= new ZPiece(bmp,this);
-
+        score=0;
+        updateScore();
     }
 
 
+    public void updateScore(){
+        score+=lineScore;
+    }
+
+    public void randomPiece(Bitmap bmp){
+        activePiece=nextPiece;
+        int piece = (int)(Math.random()*7);//Cuando se resuelva lo de lp2 poner 7 y quitar comentario de abajo
+        ImageView iv = (ImageView) findViewById(R.id.vistaImagen);
+        switch(piece){
+            case 0:
+                nextPiece= new CubePiece(bmp,this);
+                piezas.add(nextPiece);
+            case 1:
+                nextPiece= new CubePiece(bmp,this);
+                piezas.add(nextPiece);
+            case 2:
+                nextPiece = new SPiece(bmp,this);
+                piezas.add(nextPiece);
+            case 3:
+                nextPiece = new TPiece(bmp,this);
+                piezas.add(nextPiece);
+            case 4:
+                nextPiece = new ZPiece(bmp,this);
+                piezas.add(nextPiece);
+            case 5:
+                nextPiece = new JPiece(bmp,this);
+                piezas.add(nextPiece);
+            case 6:
+                nextPiece = new LPiece(bmp,this);
+                piezas.add(nextPiece);
+        }
+        if(activePiece==null){
+            randomPiece(bmp);
+        }
+    }
+
     @Override
     protected void onDraw(Canvas canvas){
-        lp.onDraw(canvas);
-        lp.changeYSpeed(300);
-        lp.rotate90Right();
-        lp.onDraw(canvas);
-        lp.changeYSpeed(300);
-        lp.rotate90Right();
-        lp.onDraw(canvas);
-
+        super.onDraw(canvas);
+        tp.onDraw(canvas);
     }
 
 
--- app/src/main/java/com/mainpakage/sprites/MainActivity.java
+++ app/src/main/java/com/mainpakage/sprites/MainActivity.java
@@ -1,32 +1,19 @@
 package com.mainpakage.sprites;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    View v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        v = (View) findViewById(R.id.view);
-
-        CustomView cv = new CustomView(this);
-
-        v = cv;
-
         setContentView(R.layout.activity_main);
     }
-
-
-
-
-
-
-
-
 }
