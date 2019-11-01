0792a7f0070302751e5c6a63d9edd3878e5e28ee
dimonra13 <diegomontoto4@hotmail.com>
2019-11-01 21:20:01 +0100

    Bugs Fixed

--- app/src/main/java/com/mainpakage/sprites/CustomView.java
+++ app/src/main/java/com/mainpakage/sprites/CustomView.java
@@ -68,8 +68,7 @@ public class CustomView extends View {
         return activePiece;
     }
 
-    public TetrixPiece getSecondPiece() {
-        return secondPiece; }
+    public TetrixPiece getSecondPiece() { return secondPiece; }
 
     public void resetSecondPiece(){
         secondPiece=null;
@@ -192,12 +191,8 @@ public class CustomView extends View {
         canvas.drawLine(0,top-cubelength,cwidth,top-cubelength,paint1);
         activePiece.onDraw(canvas);
         if(secondPiece!=null){
-            ma.enableSwitch();
             secondPiece.onDraw(canvas);
-        }else{
-            ma.disableSwitch();
         }
-
     }
 
 
